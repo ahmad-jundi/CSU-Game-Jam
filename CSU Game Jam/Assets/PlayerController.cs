54cc5243985757bca5d924743b4a00f9893f608c
Qsis <kkiussis@gmail.com>
2023-03-25 04:46:08 -0400

    Update PlayerController.cs

--- CSU Game Jam/Assets/PlayerController.cs
+++ CSU Game Jam/Assets/PlayerController.cs
@@ -24,22 +24,46 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
-    private void FixedUpdate(){
+    private void FixedUpdate()
+    {
         // If movement input is not 0, try to move
-        if(movementInput != Vector2.zero){
-            int count = rb.Cast(
+        if(movementInput != Vector2.zero)
+        {
+            bool success = TryMove(movementInput);
+
+            if(!success)
+            {
+                success = TryMove(new Vector2(movementInput.x, 0));
+
+                if(!success)
+                {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+            }
+        }
+    }
+
+    private bool TryMove(Vector2 direction)
+    {
+        int count = rb.Cast(
                 movementInput, // X and Y values between -1 and 1 that represent the direction from the body to look for collisions
                 movementFilter, // The settings that can determine where a collision can occur on such as layers to collide with
                 castCollisions, // List of collisions to store the found collisions into after the Cast is finished
                 movespeed * Time.fixedDeltaTime + collisionOffset); // The amount to cast equal to the movement plus an offset
 
-            if (count == 0){
+            if (count == 0)
+            {
                 rb.MovePosition(rb.position + movementInput * movespeed * Time.fixedDeltaTime);
+                return true;
             }
-        }
+            else 
+            {
+            return false;
+            }
     }
 
-    void OnMove(InputValue movementValue) {
+    void OnMove(InputValue movementValue) 
+    {
         movementInput = movementValue.Get<Vector2>();
     }
 }
